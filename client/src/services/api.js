7792e8e9675ef6380b52db8d0dd65bffc36d2756
Lekhya-18 <pittalekhya.24.cse@anits.edu.in>
2026-03-09 21:13:59 +0530

    Connecting FE with BE -rendering

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://future-fs-02-r19k.onrender.com',
     headers: {
         'Content-Type': 'application/json'
     }
